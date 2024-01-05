ce4bcf419fdaaf02202c19bf5af39b91a3dfd0fd
Hu Xiaohong <xiaohong@pandas.run>
2024-01-05 21:07:45 +0800

    fixed signup form validation

--- src/forms/registration_form.py
+++ src/forms/registration_form.py
@@ -6,4 +6,16 @@ class RegistrationForm(FlaskForm):
     invite_code = StringField("Invite Code", [validators.DataRequired()], name="inviteCode")
     email = StringField("Email", [validators.DataRequired(), validators.Email()])
     name = StringField("Name", [validators.DataRequired()])
-    password = PasswordField("Password", [validators.DataRequired()])
+    password = PasswordField("Password", validators=[
+        validators.DataRequired(),
+        # password at least 8 length and contains at least one letter and one number 
+        validators.length(min=8, max=20, message="Password length must be between 8 and 20"),
+        validators.Regexp("(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]+",
+                          message="Password must be at least 8 characters long and \
+                              contain at least one letter and one number")
+    ])
+    confirm_password = PasswordField("Confirm Password", validators=[
+        validators.DataRequired(), 
+        validators.EqualTo("password", message="Passwords must match"),
+        ], name="confirmPassword"
+    )
