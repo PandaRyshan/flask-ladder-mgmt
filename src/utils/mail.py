cff117dcc1ae00735347e700b118efddebfe0d0d
Hu Xiaohong <xiaohong@pandas.run>
2024-03-02 03:03:55 +0800

    refactor moved contants from dto to /root dir

--- src/utils/mail.py
+++ src/utils/mail.py
@@ -1,16 +1,24 @@
-from flask import render_template
 from flask_mailman import EmailMultiAlternatives
+from flask_security.mail_util import MailUtil
 from src.extensions.celery import celery
 
 
-@celery.task(name="send_mail", bind=True, max_retries=3, default_retry_delay=60)
-def send_mail(self, to, subject, template, **kwargs):
-    text_content = render_template(f"{template}.txt", **kwargs)              
-    html_content = render_template(f"{template}.html", **kwargs)
-    msg = EmailMultiAlternatives(
-        subject=subject,
-        to=[to],
-        body=text_content
-    )
-    msg.attach_alternative(html_content, "text/html")
+@celery.task(name="send_flask_mail", bind=True, max_retries=3, default_retry_delay=60)
+def send_flask_mail(self, **kwargs):
+    html = kwargs.pop("html", None)
+    msg = EmailMultiAlternatives(**kwargs)
+    if html:
+        msg.attach_alternative(html, "text/html")
     msg.send()
+    
+
+class SecurityMail(MailUtil):
+
+    def send_mail(self, template, subject, recipient, sender, body, html, **kwargs):
+        send_flask_mail.delay(
+            subject=subject,
+            from_email=sender,
+            to=[recipient],
+            body=body,
+            html=html
+        )
